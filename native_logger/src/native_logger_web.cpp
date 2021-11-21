6b8bbb14e7bdcaf684c293fec81d4ae35f3d234c
Salavat <salavat.rahmanov@gmail.com>
2021-11-22 00:14:07 +0600

    Native logger

--- native_logger/src/native_logger_web.cpp
+++ native_logger/src/native_logger_web.cpp
@@ -6,10 +6,14 @@ void NativeLogger_LogInternal(int severity, const char* message)
 {
     EM_ASM({
         try {
-            if ($1 != 4 && $1 != 5)
-                console.log(UTF8ToString($0));
-            else
+            if ($1 == 1)
+                console.debug(UTF8ToString($0));
+            else if($1 == 3)
+                console.warn(UTF8ToString($0));
+            else if($1 == 4 || $1 == 5)
                 console.error(UTF8ToString($0));
+            else
+                console.log(UTF8ToString($0));
         } catch (err){
             console.log(err)
         }
